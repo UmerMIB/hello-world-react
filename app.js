b8883999eef800dfa75fdc9c8f2a8500916c3659
UmerMIB <mohammadumer704@gmail.com>
2020-02-17 20:34:47 -0800

    hello world

---
+++ app.js
@@ -0,0 +1,6 @@
+var what = React.createElement("h1", null, "hello world");
+var where = document.getElementById("content");
+ReactDOM.render(React.createElement("h1", null, "hello world")
+
+
+, document.getElementById("content"));
