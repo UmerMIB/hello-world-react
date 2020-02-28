9c2006736ae73df2de054935aee9ffb1e49d83fd
UmerMIB <mohammadumer704@gmail.com>
2020-02-28 10:44:59 -0800

    complete hello world

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var what = React.createElement("h1", null, "hello world");
-var where = document.getElementById("content");
-ReactDOM.render(React.createElement("h1", null, "hello world")
+// var what = React.createElement("h1", null, "hello world");
+// var where = document.getElementById("content");
+// ReactDOM.render(what, where);
 
+// var a = React.createElement(
+//   "a",
+//   { href: "https//www.google.com", target: "_blank", title: "google link" },
+//   "Google"
+// );
+// var where = document.getElementById("content");
+// ReactDOM.render(a, where);
 
-, document.getElementById("content"));
+var h1 = React.createElement("h1", null, "hello world");
+var p = React.createElement(
+  "p",
+  null,
+  "A quick bown fox jumps over the lazy dog"
+);
+var content = document.getElementById("content");
+
+ReactDOM.render(React.createElement("div", null, h1, p), content);
